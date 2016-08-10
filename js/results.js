513c7ad7377fa87c5f3703885b517f4a45a0c543
Dylan Conkle <dconkle.dev@gmail.com>
2016-08-09 23:13:29 -0500

    Added API functions

--- js/results.js
+++ js/results.js
@@ -1,30 +1,46 @@
 $(document).ready(function() {
-    $("#search-form").submit(function(event) {
-        event.preventDefault();
-        var userInput = $("#query").val();
-        if (userInput == 0) {
-            alert('Please type a search term!');
-        } else {
-            getResults(userInput);
-        };
-    });
+            $("#search-form").submit(function(event) {
+                event.preventDefault();
+                var userInput = $("#query").val();
+                if (userInput == 0) {
+                    alert('Please type a search term!');
+                } else {
+                    spotifyApiCall(userInput);
+                    youtubeApiCall(userInput);
+                };
+            });
 
-function spotifyApiCall(userInput){
+            function spotifyApiCall(userSearchTerm) {
+                $.getJSON("https://api.spotify.com/v1/search", {
+                        q: userSearchTerm,
+                        limit: 20,
+                        type: "track",
+                    },
+                    function(receivedApiData) {
+                        console.log(receivedApiData);
+                        if (receivedApiData.pageInfo.totalResults == 0) {
+                            alert("Not found!");
+                        } else {
+                            displaySearchResults(receivedApiData.items);
+                        }
+                    });
+                  }
 
-}
-
-function itunesApiCall(userInput){
-
-}
-
-function soundcloudApiCall(userInput){
-
-}
-
-function youtubeApiCall(userInput){
-
-}
-
-function googleApiCall(userInput){
-
-}
+            function youtubeApiCall(userSearchTerm) {
+                $.getJSON("https://www.googleapis.com/youtube/v3/search", {
+                        part: "snippet",
+                        maxResults: 20,
+                        key: "AIzaSyD7y-IwGNkpZIIwE5K8IymLFM6HZAYNsiY",
+                        q: userSearchTerm,
+                        type: "video"
+                    },
+                    function(receivedApiData) {
+                        console.log(receivedApiData);
+                        if (receivedApiData.pageInfo.totalResults == 0) {
+                            alert("Not found!");
+                        } else {
+                            displaySearchResults(receivedApiData.items);
+                        }
+                    });
+                  }
+});
